5ea47423ecaa8ae315b9d8e9ecaf6decdd07c88f
JaeHwan <wpghks56@naver.com>
2022-11-11 14:31:34 +0900

    2022-11-010 flutter-git search area

--- flutter_real_todo/lib/tab/tab_search_user.dart
+++ flutter_real_todo/lib/tab/tab_search_user.dart
@@ -2,8 +2,38 @@ import 'package:flutter/material.dart';
 
 class TabSearchUser extends StatelessWidget {
 
+  final searchText = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 290,
+                height: 50,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.blueGrey),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: const EdgeInsets.all(5),
+                margin: const EdgeInsets.only(bottom: 5.0),
+                child : TextField(
+                  controller: searchText,
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "검색할 유저를 입력해주세요."
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
