0902000c0970447627d6b35bed15542c7545da52
JaeHwan <wpghks56@naver.com>
2022-12-09 15:27:38 +0900

    2022-12-09 Exercise-pick input skeleton create

--- flutter_real_todo/lib/util/exercise_pick.dart
+++ flutter_real_todo/lib/util/exercise_pick.dart
@@ -1,11 +1,13 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_real_todo/util/exercise_enum.dart';
 import 'package:get/get.dart';
 
 class ExercisePick extends StatelessWidget {
   final _type = ExerciseType.back.obs;
+  final setNo = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +38,26 @@ class ExercisePick extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
+                  Container(
+                    width: 60,
+                    height: 50,
+                    padding: const EdgeInsets.only(left: 5),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        style: BorderStyle.solid,
+                      ),
+                      borderRadius: BorderRadius.circular(12)
+                    ),
+                    child: TextField(
+                      controller: setNo,
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Set.No",
 
+                      ),
+                      keyboardType: const TextInputType.numberWithOptions(),
+                    ),
+                  ),
                 ],
               ),
               SizedBox(
