5032a5228a7e0c11db9860e90648a3eb204d4b5e
JaeHwan <wpghks56@naver.com>
2022-12-15 11:58:52 +0900

    2022-12-15 exercise insert & get

--- flutter_real_todo/lib/controller/controller_exercise.dart
+++ flutter_real_todo/lib/controller/controller_exercise.dart
@@ -1,7 +1,73 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter_real_todo/model/model_exercise.dart';
 import 'package:get/get.dart';
 import 'package:meta/meta.dart';
+import 'package:http/http.dart' as http;
+import 'controller_user.dart';
 
 class ExerciseController extends GetxController {
+  final exerciseService = Get.put(ExerciseModel());
+  final userModel = Get.put(UserController());
+  final defaultUrl = "http://localhost:8080";
+  final mobileUrl = "http://10.0.2.2:8080";
+  final iosUrl = "http://127.0.0.1:8080";
 
+  Future<void> insertExercises() async {
+    var url = Uri.parse("$defaultUrl/exer");
+    try {
+      if(Platform.isAndroid) {
+        url = Uri.parse("$mobileUrl/exer");
+      } else if(Platform.isIOS) {
+        url = Uri.parse("$iosUrl/exer");
+      }
+    } catch(e) {
+      print(e);
+    }
+
+    await http.post(
+      url,
+      headers: {"Content-Type" : "application/json"},
+      body: json.encode({
+        exerciseService.toJson()
+      }),
+    );
+  }
+
+  Future<void> getExercises() async {
+    var url = Uri.parse("$defaultUrl/exer/${userModel.user.value.id}");
+    try {
+      if(Platform.isAndroid) {
+        url = Uri.parse("$mobileUrl/exer/${userModel.user.value.id}");
+      } else if(Platform.isIOS) {
+        url = Uri.parse("$iosUrl/exer/${userModel.user.value.id}");
+      }
+    } catch(e) {
+      print(e);
+    }
+
+    await http.get(
+      url
+    ).then((value) {
+      if(value.statusCode == 200) {
+        ExerciseModel.fromJson(json.decode(value.body));
+      }
+    });
+  }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ flutter_real_todo/lib/model/model_exercise.dart
@@ -0,0 +1,62 @@
+import 'package:get/get.dart';
+
+class RxExerciseModel {
+  final exid = 0.obs;
+  final exName = 'name'.obs;
+  final setNo = 0.obs;
+  final part = "back".obs;
+  final totalNo = 0.obs;
+  final setPerNo = 0.obs;
+  final breakTime = 0.obs;
+  final exDate = DateTime.now().obs;
+}
+
+class ExerciseModel {
+  ExerciseModel({id, name});
+
+  final rx = RxExerciseModel();
+
+  get exid => rx.exid.value;
+  set exid(value) => rx.exid.value = value;
+
+  get exName => rx.exName.value;
+  set exName(value) => rx.exName.value = value;
+
+  get setNo => rx.setNo.value;
+  set setNo(value) => rx.setNo.value = value;
+
+  get part => rx.part.value;
+  set part(value) => rx.part.value = value;
+
+  get totalNo => rx.totalNo.value;
+  set totalNo(value) => rx.totalNo.value = value;
+
+  get setPerNo => rx.setPerNo.value;
+  set setPerNo(value) => rx.setPerNo.value = value;
+
+  get breakTime => rx.setPerNo.value;
+  set breakTime(value) => rx.setPerNo.value = value;
+
+  get exDate => rx.exDate.value;
+  set exDate(value) => rx.exDate.value = value;
+
+  ExerciseModel.fromJson(Map<String, dynamic> json) {
+    exid = json['exid'];
+    exName = json['exName'];
+    setNo = json['setNo'];
+    part = json['part'];
+    totalNo = json['totalNo'];
+    setPerNo = json['setPerNo'];
+    breakTime = json['breakTime'];
+    exDate = json['exDate'];
+  }
+
+  Map<String, dynamic> toJson() => {
+    'exName' : exName,
+    'setNo' : setNo,
+    'part' : part,
+    'totalNo' : totalNo,
+    'setPerNo' : setPerNo,
+    'breakTime' : breakTime,
+  };
+}
